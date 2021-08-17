c9f7f5fff7f1a503b8327953e18169a886abde61
TerryL1971 <tcl71771@yahoo.com>
2021-08-17 12:02:40 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -8,14 +9,23 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myFlix-Angular-client';
+  title = 'myMovies-client-angular';
 
   constructor(public dialog: MatDialog) { }
-// This is the function that will open the dialog when the signup button is clicked  
+  // function to open the dialog when the Sign Up button is clicked  
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-// Assigning the dialog a width
-    width: '280px'
+      // Assigning the dialog a width
+      width: '280px'
     });
   }
+
+  // function to open the dialog when the Log In button is clicked
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Assigning the dialog a width
+      width: '280px'
+    });
+  }
+
 }
