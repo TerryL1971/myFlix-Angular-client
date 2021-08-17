46e57730002cf780228478c60dd6a2d29814dd6a
TerryL1971 <tcl71771@yahoo.com>
2021-08-17 12:02:44 +0200

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -8,7 +8,7 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-const apiUrl = 'https://myflix-app-2021.herokuapp.com/';
+const apiUrl = 'https://myflix-app-2021.herokuapp.com/movies';
 
 @Injectable({
   providedIn: 'root',
@@ -31,6 +31,37 @@ export class UserRegistrationService {
       );
     }
     return throwError('Something went wrong - please try again later!');
+  }
+}
+
+// User login
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserLoginService {
+  constructor(private http: HttpClient) { }
+
+  public userLogin(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .post(apiUrl + 'login', userDetails)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status},` +
+        `Error body is: ${error.error}`);
+    }
+    return throwError(
+      'Something went wrong - please try again later!'
+    );
   }
 }
 
