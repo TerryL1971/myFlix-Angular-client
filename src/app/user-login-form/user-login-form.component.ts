4ce8ab0f622293560ea3e586cc16a7b098248876
TerryL1971 <tcl71771@yahoo.com>
2021-08-18 11:20:55 +0200

    Update user-login-form.component.ts

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -31,6 +31,9 @@ export class UserLoginFormComponent implements OnInit {
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
       // Logic for a successful user login goes here! (To be implemented)
       this.dialogRef.close(); // This will close the modal on success!
+      localStorage.setItem('user', result.user.Username);
+      localStorage.setItem('token', result.token);
+      localStorage.setItem('FavoriteMovies', result.user.FavoriteMovies);
       console.log(result);
       this.snackBar.open(result, 'OK', {
         duration: 2000
