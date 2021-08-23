d490fb01098849271496c7108c5cfab003ceeb6a
TerryL1971 <tcl71771@yahoo.com>
2021-08-23 11:45:29 +0200

    Update movie-card.component.ts

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { GetAllMoviesService } from '../fetch-api-data.service';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { MovieDescriptionViewComponent } from '../movie-description-view/movie-description-view.component';
+import { GenreViewComponent } from '../genre-view/genre-view.component';
+import { DirectorViewComponent } from '../director-view/director-view.component';
 
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -13,7 +17,7 @@ export class MovieCardComponent {
   movies: any[] = [];
 
 constructor(
-  public fetchApiData: GetAllMoviesService,
+  public fetchApiData: FetchApiDataService,
   public dialog: MatDialog,
   public snackBar: MatSnackBar
 ) { }
@@ -22,11 +26,35 @@ ngOnInit(): void {
   this.getMovies();
 }
 
+// Gets Movie API
 getMovies(): void {
   this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-      this.movies = resp;
-      console.log(this.movies);
-      return this.movies;
-    });
-  }
+    this.movies = resp;
+    console.log(this.movies);
+    return this.movies;
+  });
+}
+//open movie details
+openGenreViewDialog(name: string, description: string): void {
+  this.dialog.open(GenreViewComponent, {
+    data: { name, description },
+    width: '300px',
+  });
+}
+
+//open movie details
+openDirectorViewDialog(name: string, bio: string, image: string): void {
+  this.dialog.open(DirectorViewComponent, {
+    data: { name, bio, image },
+    width: '300px',
+  });
+}
+
+//open movie description
+openMovieDescriptionViewDialog(description: string): void {
+  this.dialog.open(MovieDescriptionViewComponent, {
+    data: { description },
+    width: '300px',
+  });
+}
 }
