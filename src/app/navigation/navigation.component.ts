15d6297da0957b24649d53ca5a1ae112dde54c4c
TerryL1971 <tcl71771@yahoo.com>
2021-08-24 16:55:38 +0200

    Update navigation.component.ts

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -4,17 +4,22 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss'],
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-  constructor(public snackBar: MatSnackBar) {}
 
-  ngOnInit(): void {}
+  constructor(
+    public snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+  }
 
   logOut(): void {
     localStorage.clear;
     this.snackBar.open('You have logged out!', 'Ok', {
-      duration: 2000,
+      duration: 2000
     });
   }
+
 }
