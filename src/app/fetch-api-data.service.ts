38bf12f0167a63985fbae664d803752dcd3e699e
TerryL1971 <tcl71771@yahoo.com>
2021-08-27 08:49:00 +0200

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,6 +9,7 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+//Declaring the api url that will provide data for the client app
 const apiUrl = 'https://myflix-app-2021.herokuapp.com/';
 
 @Injectable({
